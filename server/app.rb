bcd67bad5691cdc0a795a6ddc2ba096d3adea8fc
Auclair Emmanuel <auclair.emmanuel@gmail.com>
2015-01-24 01:19:32 +0100

    Use GIT_HEAD to version appcache

--- server/app.rb
+++ server/app.rb
@@ -4,6 +4,8 @@ require 'json'
 class SRApp < Sinatra::Base
 
   def initialize
+    @git_commit = ENV['GIT_HEAD']
+    # @git_commit = `git rev-parse --short HEAD`
     super
   end
 
@@ -12,8 +14,20 @@ class SRApp < Sinatra::Base
   set :static_cache_control, [:no_cache, :must_revalidate]
   set :views, File.join(File.dirname(__FILE__), '..', 'client')
 
+  before do
+    expires 0
+    cache_control :no_cache, :must_revalidate
+  end
+
   get '/' do
     redirect '/index.html'
   end
 
+  get '/manifest.appcache' do
+    erb :manifest_appcache
+  end
+
+  get '/title.html' do
+    erb :title
+  end
 end
